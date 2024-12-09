b39b3442652f535336889e00d068806392e6f673
CristianLupascu <lupascugabrielcristian@yahoo.com>
2024-12-09 21:49:53 +0200

    colors in presentation from theme

--- lib/screens/presentation/presentation.dart
+++ lib/screens/presentation/presentation.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ratemy/screens/presentation/app_theme.dart';
 
 class Presentation {
-
+  final ThemeData theme = AppTheme.getAppTheme();
+ 
   get background => Colors.white;
+  get primary => theme.colorScheme.primary;
 
   Widget get gapAboveScreenTitle {
     return const SizedBox(height: 35);
